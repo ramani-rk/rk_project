4a85a6d187f78cfc0360ae28a61cc1394176d6c3
ramani-rk <ramanikanthsasi@gmail.com>
2023-11-08 17:16:03 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def rk (request):
+    return HttpResponse('Hiii RK...How r u!!!!!')
+
+def srk (request):
+    return HttpResponse(<h1><marquee>'Hiii im Good'</h1></marquee>)
